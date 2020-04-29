4f654cc2894fbd4ece8430a97985deeab5b2b1c6
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-04-29 10:15:43 -0700

    Updates

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function createOutput(words){
         outterSpan.classList.add('outterSpan');
         // parse string
         chars.forEach((char)=>{
-            console.log(char);
+            // console.log(char);
             var innerSpan = document.createElement('span');
             innerSpan.classList.add('innerSpan');
             innerSpan.innerText = char;
@@ -27,17 +27,73 @@ function clearInput(){
     input.value = '';
 }
 
-input.addEventListener('input',(e)=>{
-    console.log(e.target.value);
+var errCount = 0;
+var reservedKeys = ['Backspace','Shift']
+
+input.addEventListener('keydown',(e)=>{
+    // console.log(e.target.value);
+    // console.log(e.key)
+    console.log(e.key)
     let value = e.target.value;
     let currentWord = words[0];
+    let currentChar = document.querySelector('.innerSpan');
+    let inputChar = e.key;
+    
+
+    let wordLength = currentWord.length;
+    let inputLength = value.length;
+
+
+
+    console.log(currentWord.length)
 
     if(value === currentWord){
-        console.log('match');
+
         words.shift()
         // console.log('new word: ..... ',words[0])
         clearInput();
+    };
+    // if(!(inputChar in reservedKeys)){
+    //     console.log('fkdlsjfkld;ajlkfals;jfl;dajsk;!!!FRICKKKK')
+    //     console.log(reservedKeys)
+    //     console.log(inputChar)
+    // }
+
+    
+    if(inputChar === currentChar.innerText && inputChar!='Shift' && inputChar!='Backspace' && errCount < 1){
+
+        currentChar.style.color = 'green';
+
+        currentChar.classList.remove('innerSpan');
+
+
+    }else if(inputChar != currentChar.innerText && inputChar != 'Backspace'){
+        currentChar.style.color = 'red';
+        errCount += 1;
+    }else if(inputChar == 'Backspace'){
+        console.log('Backspace');
+        errCount -= 1;
     }
+    console.log('%c ERR CNT: ','color: yellow',errCount)
+
+
 });
+
+input.addEventListener('keypress',(e)=>{
+    console.log(e.key)
+})
+// window.addEventListener("keydown", function(event) {
+//     let str = "KeyboardEvent: key='" + event.key + "' | code='" +
+//               event.code + "'";
+//     let el = document.createElement("span");
+//     el.innerHTML = str + "<br/>";
+   
+//     document.getElementById("output").appendChild(el);
+//   }, true);
+
+// input.on('keypress',()=>{
+//     // console.log()
+// })
+
 
 createOutput(words);
